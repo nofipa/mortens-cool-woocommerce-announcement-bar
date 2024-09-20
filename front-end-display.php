d98fae785e6865921336c5e65132110ba03831d9
Voldemorten <mortendlaursen@protonmail.com>
2024-09-20 10:15:09 +0200

    specifics for pantsat.dk

--- front-end-display.php
+++ front-end-display.php
@@ -1,5 +1,6 @@
 <?php
-function mcab_display_announcement_bar() {
+function mcab_display_announcement_bar()
+{
     $options = get_option('mcab_settings');
     // Check if 'mcab_field_enable' is set and true
     if (isset($options['mcab_field_enable']) && $options['mcab_field_enable']) {
@@ -7,6 +8,19 @@ function mcab_display_announcement_bar() {
         echo '<div id="mortens-cool-announcement-bar" style="text-align: center; color: ' . esc_attr($options['mcab_field_text_color']) . '; background-color: ' . esc_attr($options['mcab_field_background_color']) . '; font-size: ' . esc_attr($options['mcab_field_text_size']) . ';">';
         echo $options['mcab_field_content'];
         echo '</div>';
+
+        // Add JavaScript to check .site_header_wrap and adjust its parent if needed
+        echo '<script>
+            document.addEventListener("DOMContentLoaded", function() {
+                const headerWrap = document.querySelector(".site_header_wrap");
+                if (headerWrap && window.getComputedStyle(headerWrap).position === "absolute") {
+                    const parent = headerWrap.parentElement;
+                    if (parent) {
+                        parent.style.position = "relative";
+                    }
+                }
+            });
+        </script>';
     }
 }
 
